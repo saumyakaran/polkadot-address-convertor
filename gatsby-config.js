0d2706123c6e8b74f5d5bc56eac5e7490f59ab56
saumyakaran <40575379+saumyakaran@users.noreply.github.com>
2020-12-12 13:22:59 +0530

    fix path

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#1A202C`,
         display: `minimal-ui`,
-        icon: `src/images/polka-analytics.png`, // This path is relative to the root of the site.
+        icon: `src/images/polka-analytics.svg`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
